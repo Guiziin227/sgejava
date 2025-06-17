94e03433d52b663e0e8488d8e2f863983d798b74
Guiziin227 <gh63388@gmail.com>
2025-06-17 11:26:21 -0300

    controller e service

---
+++ backend/sgejava/src/main/java/com/github/guiziin227/sgejava/service/UserService.java
@@ -0,0 +1,35 @@
+package com.github.guiziin227.sgejava.service;
+
+import com.github.guiziin227.sgejava.model.User;
+import com.github.guiziin227.sgejava.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User createUser(User user) {
+        logger.info("Creating user");
+        return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        logger.info("Retrieving all users");
+        return userRepository.findAll();
+    }
+
+
+}
