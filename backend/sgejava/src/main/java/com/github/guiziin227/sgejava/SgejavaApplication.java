a9e353d1abacb901f5e8f2eb0404fec3ce0be211
Guiziin227 <gh63388@gmail.com>
2025-06-17 10:31:16 -0300

    backend first commit

---
+++ backend/sgejava/src/main/java/com/github/guiziin227/sgejava/SgejavaApplication.java
@@ -0,0 +1,13 @@
+package com.github.guiziin227.sgejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgejavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgejavaApplication.class, args);
+    }
+
+}
---
+++ backend/sgejava/src/test/java/com/github/guiziin227/sgejava/SgejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guiziin227.sgejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgejavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
