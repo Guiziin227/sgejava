6d2a25d833a0fe349bc69ca5008fb2f15d0c43e5
schergds <schergds@gmail.com>
2025-06-17 17:10:02 -0300

    criação de evento e suas dependências

---
+++ backend/sgejava/src/main/java/com/github/guiziin227/sgejava/repository/EventoRepository.java
@@ -0,0 +1,7 @@
+package com.github.guiziin227.sgejava.repository;
+
+import com.github.guiziin227.sgejava.model.Evento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventoRepository extends JpaRepository<Evento, Long> {
+}
