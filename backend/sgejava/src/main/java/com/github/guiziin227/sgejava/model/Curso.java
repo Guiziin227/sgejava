6bcd4a6625185bbab74b64792116424e2d564b62
Guiziin227 <gh63388@gmail.com>
2025-06-17 11:55:03 -0300

    Create curso model

---
+++ backend/sgejava/src/main/java/com/github/guiziin227/sgejava/model/Curso.java
@@ -0,0 +1,37 @@
+package com.github.guiziin227.sgejava.model;
+
+import com.github.guiziin227.sgejava.model.enums.AreasConhecimento;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_curso")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AreasConhecimento areasConhecimento;
+}
